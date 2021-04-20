57ba1d49335a683ec579c153befa8cbd6dab8761
Nassos Katsamanis <nkatsam@gmail.com>
2021-04-20 22:45:10 +0300

    Fix missing command error

--- asr-recipes/mini_librispeech/s5/analyze_results.sh
+++ asr-recipes/mini_librispeech/s5/analyze_results.sh
@@ -3,8 +3,8 @@ ref_trw=exp/tri3b/decode_tgsmall_dev_clean_2/scoring/test_filt.txt
 
 cat $hypothesis_tra | utils/int2sym.pl -f 2- exp/tri3b/graph_tgsmall/words.txt | sed s:\<UNK\>::g > hypothesis.trw
 
-python hypothesis.trw hypothesis.wsj
-python $ref_trw ref.wsj
+python trw_to_wsj.py hypothesis.trw hypothesis.wsj
+python trw_to_wsj.py $ref_trw ref.wsj
 
 
 
